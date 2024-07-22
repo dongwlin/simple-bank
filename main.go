fdfbcd824ef36af70ed9fd0efe7da06a22130a34
dongwlin <dongwlin@foxmail.com>
2024-07-22 16:15:51 +0800

    fix: create server error log content

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server, err := api.NewServer(config, store)
 	if err != nil {
-		log.Fatal("cannot  create token maker:", err)
+		log.Fatal("cannot create server:", err)
 	}
 
 	err = server.Start(config.ServerAddress)
